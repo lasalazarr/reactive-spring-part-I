18ff467e13d20089ea4e8af681e399fe2d173a0c
Alberto Salazar <alberto.salazar@ac-techsolutions.com>
2018-09-19 10:55:23 -0500

    Simple Springboot application with Springwebflux dependency
    Simple Springboot application with Springwebflux dependency

---
+++ src/main/java/com/otn/reactive/reactivespringpartI/ReactiveSpringPartIApplication.java
@@ -0,0 +1,12 @@
+package com.otn.reactive.reactivespringpartI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveSpringPartIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveSpringPartIApplication.class, args);
+	}
+}
---
+++ src/test/java/com/otn/reactive/reactivespringpartI/ReactiveSpringPartIApplicationTests.java
@@ -0,0 +1,16 @@
+package com.otn.reactive.reactivespringpartI;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReactiveSpringPartIApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
