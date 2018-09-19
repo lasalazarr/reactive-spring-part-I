cd5801bf4d78c248447401982e2abd76ed396313
Alberto Salazar <alberto.salazar@ac-techsolutions.com>
2018-09-19 17:43:10 -0500

    Fix lista de usuarios request
    Fix lista de usuarios request

--- src/main/java/com/otn/reactive/reactivespringpartI/Cliente.java
+++ src/main/java/com/otn/reactive/reactivespringpartI/Cliente.java
@@ -2,6 +2,72 @@ package com.otn.reactive.reactivespringpartI;
 
 public class Cliente {
 
+    private String id;
 
+    private String nombres;
 
+    private String apellidos;
+
+    private Integer telefono;
+
+    private String direccion;
+
+    public Cliente(String id, String nombres, String apellidos, Integer telefono, String direccion) {
+        this.id = id;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.telefono = telefono;
+        this.direccion = direccion;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public Integer getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(Integer telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "id='" + id + '\'' +
+                ", nombres='" + nombres + '\'' +
+                ", apellidos='" + apellidos + '\'' +
+                ", telefono=" + telefono +
+                ", direccion='" + direccion + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/otn/reactive/reactivespringpartI/ClienteHandler.java
+++ src/main/java/com/otn/reactive/reactivespringpartI/ClienteHandler.java
@@ -7,17 +7,22 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
 public class ClienteHandler {
 
-    /**
-     * GET ALL Customers
-     */
     public Mono<ServerResponse> getAll(ServerRequest request) {
-        // fetch all customers from repository
-        Flux<Cliente> clientes = null;//Flux.fromIterable(customerRepository.findAll());
+        // Simulamos un repositorio en memoria creando una lista de clientes.
+        List<Cliente> clientesRepositorio = new ArrayList<Cliente>();
+        clientesRepositorio.add(new Cliente("1001", "Alberto", "Salazar", 3299999, "Quito, EC170102"));
+        clientesRepositorio.add(new Cliente("1002", "Pablo", "Arizaga", 22332233, "Quito, EC170104"));
+        clientesRepositorio.add(new Cliente("1003", "Maria", "Valdez", 82923233, "Quito, EC178976"));
+        clientesRepositorio.add(new Cliente("1004", "Paola", "Llanos", 87878799, "Quito, EC179087"));
+        clientesRepositorio.add(new Cliente("1005", "Tamara", "Cisneros", 4356772, "Quito, EC876590"));
 
-        // build response
+        Flux<Cliente> clientes = Flux.fromIterable(clientesRepositorio);
         return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).body(clientes, Cliente.class);
     }
 }
