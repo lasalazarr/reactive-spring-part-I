90ff79c86bef38667e8438e0ad57887ad34aafce
Alberto Salazar <alberto.salazar@ac-techsolutions.com>
2018-09-21 10:34:27 -0500

    Mock repository
    Mock repository

--- src/main/java/com/otn/reactive/reactivespringpartI/ClienteHandler.java
+++ src/main/java/com/otn/reactive/reactivespringpartI/ClienteHandler.java
@@ -1,5 +1,6 @@
 package com.otn.reactive.reactivespringpartI;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
@@ -13,16 +14,13 @@ import java.util.List;
 @Component
 public class ClienteHandler {
 
-    public Mono<ServerResponse> getAll(ServerRequest request) {
-        // Simulamos un repositorio en memoria creando una lista de clientes.
-        List<Cliente> clientesRepositorio = new ArrayList<Cliente>();
-        clientesRepositorio.add(new Cliente("1001", "Alberto", "Salazar", 3299999, "Quito, EC170102"));
-        clientesRepositorio.add(new Cliente("1002", "Pablo", "Arizaga", 22332233, "Quito, EC170104"));
-        clientesRepositorio.add(new Cliente("1003", "Maria", "Valdez", 82923233, "Quito, EC178976"));
-        clientesRepositorio.add(new Cliente("1004", "Paola", "Llanos", 87878799, "Quito, EC179087"));
-        clientesRepositorio.add(new Cliente("1005", "Tamara", "Cisneros", 4356772, "Quito, EC876590"));
+    @Autowired
+    MockClienteRepository mockClienteRepository;
 
-        Flux<Cliente> clientes = Flux.fromIterable(clientesRepositorio);
+    public Mono<ServerResponse> getAll(ServerRequest request) {
+        Flux<Cliente> clientes = Flux.fromIterable(mockClienteRepository.getAll());
         return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).body(clientes, Cliente.class);
     }
+
+
 }
---
+++ src/main/java/com/otn/reactive/reactivespringpartI/MockClienteRepository.java
@@ -0,0 +1,41 @@
+package com.otn.reactive.reactivespringpartI;
+
+import org.springframework.stereotype.Repository;
+
+import javax.annotation.PostConstruct;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@Repository
+public class MockClienteRepository {
+
+    private Map<String, Cliente> clientesStore = new HashMap<String, Cliente>();
+
+    @PostConstruct
+    public void initIt() throws Exception {
+        clientesStore.put("1001", new Cliente("1001", "Alberto", "Salazar", 3299999, "Quito, EC170102"));
+        clientesStore.put("1002", new Cliente("1002", "Pablo", "Arizaga", 22332233, "Quito, EC170104"));
+        clientesStore.put("1003", new Cliente("1003", "Maria", "Valdez", 82923233, "Quito, EC178976"));
+        clientesStore.put("1004", new Cliente("1004", "Paola", "Llanos", 87878799, "Quito, EC179087"));
+        clientesStore.put("1005", new Cliente("1005", "Tamara", "Cisneros", 4356772, "Quito, EC876590"));
+    }
+
+    public Collection<Cliente> getAll() {
+        return clientesStore.values();
+    }
+
+    public Cliente save( Cliente cliente){
+        clientesStore.put(cliente.getId(), cliente);
+        return cliente;
+    }
+
+    public Cliente update(Cliente cliente){
+        clientesStore.put(cliente.getId(), cliente);
+        return cliente;
+    }
+
+    public void delete(String id){
+        clientesStore.remove(id);
+    }
+}
